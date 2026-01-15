aace32399e4c37738eaed644b55f9ce5d3e97b70
Emirhan yiğit Yıldırım <emirhanyigityildirim0@gmail.com>
2026-01-15 22:13:01 +0300

    MainMenu

--- Scripts/UI/point_light_2d.gd
+++ Scripts/UI/point_light_2d.gd
@@ -5,7 +5,7 @@ var hedef_enerji = 1.0
 func _process(delta):
 	# Işığın gücünü rastgele değiştir (Titreme efekti)
 	if randf() < 0.1: # Her karede %10 şansla yeni bir güç belirle
-		hedef_enerji = randf_range(1, 2) # 0.8 ile 1.2 arasında gidip gelsin
+		hedef_enerji = randf_range(0.8, 2) # 0.8 ile 1.2 arasında gidip gelsin
 	
 	# Yumuşak geçiş yap (Küt diye değişmesin)
 	energy = move_toward(energy, hedef_enerji,1* delta)
